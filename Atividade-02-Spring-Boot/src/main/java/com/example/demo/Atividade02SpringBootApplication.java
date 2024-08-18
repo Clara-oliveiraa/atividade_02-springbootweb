98c2b23906a35109e1017f50b786a53cfd736816
Clara-oliveiraa <claraoliveira.8351@gmail.com>
2024-08-18 19:47:50 -0300

    Atividade SpringBoot 02

---
+++ Atividade-02-Spring-Boot/src/main/java/com/example/demo/Atividade02SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividade02SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividade02SpringBootApplication.class, args);
+	}
+
+}
